04e5fd3454657cb91fa06d3265b72975075bcb32
Rachel <rachelsales2011@hotmail.com>
2022-12-16 17:05:48 -0300

    terinando operadores

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int idade = 26;
+        System.out.println("Idade é: " + idade);
+        idade += 1;
+        System.out.println ("Idade é: " + idade);
+        String resultado = idade > 30 ? "velho" : "novo";
+        System.out.println(resultado);
+
+        boolean simNao = numero1 == numero2;
+        
+
+    }
+}
